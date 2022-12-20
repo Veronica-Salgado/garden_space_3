e800d69b7d986cd0e60d59888c74902c32db6478
Veroncia_Salgado <veronicas1327@gmail.com>
2022-12-20 20:30:47 +0000

    finished page 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,73 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+void main() => runApp(MaterialApp(
+      theme: ThemeData.dark().copyWith(
+          elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(Colors.teal.shade600),
+      ))),
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              const Spacer(
+                flex: 2,
+              ),
+              Text(
+                'Garden Space',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
+                  color: Colors.teal.shade600,
+                ),
+              ), //Garden Space
+              Text(
+                'Log In',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25,
+                  color: Colors.teal.shade600,
+                ),
+              ), //Log In
+              const Spacer(
+                flex: 2,
+              ),
+              TextField(
+                  style: TextStyle(color: Colors.teal.shade100),
+                  decoration:
+                      const InputDecoration(labelText: 'Email or Username')),
+              const Spacer(),
+              const TextField(
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                ),
+              ),
+              const Spacer(),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Log In'),
+              ),
+              const Spacer(),
+              TextButton(
+                onPressed: () {},
+                child: const Text('Sign Up'),
+              ), //Sign Up
+              const Spacer(),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Log in with Google'),
+              ),
+              const Spacer(),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Log in With Facebook'),
+              ),
+              const Spacer()
+            ],
+          ),
         ),
       ),
-    );
-  }
-}
+    ));
