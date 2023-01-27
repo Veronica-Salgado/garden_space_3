0e6165e837f9070a3f596c9dba8161c0a6719477
Veroncia_Salgado <veronicas1327@gmail.com>
2023-01-27 20:21:33 +0000

    Addition of the create account screen and the home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,74 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:helloworld/create_account.dart';
+import 'package:helloworld/login_screen.dart';
+import 'package:helloworld/home.dart';
 
 void main() => runApp(MaterialApp(
       theme: ThemeData.dark().copyWith(
           elevatedButtonTheme: ElevatedButtonThemeData(
               style: ButtonStyle(
-        backgroundColor: MaterialStateProperty.all(Colors.teal.shade600),
-      ))),
-      home: Scaffold(
-        backgroundColor: Colors.black,
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              const Spacer(
-                flex: 4,
-              ),
-              Text(
-                'Create Account',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 30,
-                  color: Colors.teal.shade600,
-                ),
-              ), //Garden Space
-              const Spacer(
-                flex: 4,
-              ),
-              TextField(
-                  style: TextStyle(color: Colors.teal.shade100),
-                  decoration:
-                      const InputDecoration(labelText: 'Enter your Username')),
-              const Spacer(),
-              const TextField(
-                decoration: InputDecoration(
-                  labelText: 'Enter your Email',
-                ),
-              ),
-              const Spacer(),
-              TextField(
-                style: TextStyle(color: Colors.teal.shade100),
-                decoration:
-                    const InputDecoration(labelText: 'Enter Your Phone Number'),
-              ),
-              const Spacer(),
-              TextField(
-                style: TextStyle(color: Colors.teal.shade100),
-                decoration:
-                    const InputDecoration(labelText: 'Enter Your Password'),
-              ),
-              const Spacer(
-                flex: 4,
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                child: const Text('Sign Up'),
-              ),
-              const Spacer(),
-              ElevatedButton(
-                onPressed: () {},
-                child: const Text('Sign up with Google'),
-              ),
-              const Spacer(),
-              ElevatedButton(
-                onPressed: () {},
-                child: const Text('Sign up With Facebook'),
-              ),
-              const Spacer()
-            ],
-          ),
-        ),
-      ),
+            backgroundColor: MaterialStateProperty.all(Colors.teal.shade600),
+          )),
+          textButtonTheme: TextButtonThemeData(
+              style: ButtonStyle(
+                  textStyle:
+                      MaterialStateProperty.all(const TextStyle(fontSize: 17)),
+                  foregroundColor:
+                      MaterialStateProperty.all(Colors.teal.shade100)))),
+      title: 'Named Routes Demo',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const LogInScreen(),
+        '/second': (context) => const CreateAccount(),
+        '/third': (context) => const Home(),
+      },
     ));
