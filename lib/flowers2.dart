782799c7036d284361c8b1b8c1c8abff0b98f0b8
aaronwhitfield <aawhitfield@hotmail.com>
2023-02-22 13:10:35 -0800

    creates start of flower list

--- lib/flowers2.dart
+++ lib/flowers2.dart
@@ -1,1 +1,23 @@
+// list of 100 common flowers
+List<String> flowerList = [
+  "African Daisy",
+  "African Violet",
+  "Agapanthus",
+  "Ageratum",
+  "Alyssum",
+  "Amaryllis",
+  "Anemone",
+  "Angel's Trumpet",
+  "Anthurium",
+  "Apple Blossom",
+  "Arum Lily",
+  "Asiatic Lily",
+  "Aster",
+  "Azalea",
+  "Baby's Breath",
+  "Bachelor's Button",
+  "Begonia",
+  "Bellflower",
 
+
+];
